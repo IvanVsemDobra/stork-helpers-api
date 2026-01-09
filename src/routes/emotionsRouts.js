0721f3a26e735a58c5199397b649b94e2fa42eca
Anton Zorkin <zanton1978@gmail.com>
2026-01-09 09:35:26 +0100

    fix

--- src/routes/emotionsRouts.js
+++ src/routes/emotionsRouts.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
+import { getEmotions } from '../controllers/emotionsController.js';
 
 const router = Router();
 
-router.get('/emotions');
+router.get('/emotions', getEmotions);
 export default router;
