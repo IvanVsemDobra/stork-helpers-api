c1ae63feecf1654268027d3b7ceb6c98e9a84b84
Jeka <gekanefed22.04.2010@gmail.com>
2026-01-18 23:57:35 +0200

    add cookie-based session authentication middleware

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,4 +1,5 @@
 import createHttpError from 'http-errors';
+import mongoose from 'mongoose';
 import { Session } from '../models/session.js';
 import { User } from '../models/user.model.js';
 
@@ -10,16 +11,17 @@ export const authenticate = async (req, res, next) => {
       return next(createHttpError(401, 'Not authenticated'));
     }
 
-    const session = await Session.findOne({
-      _id: sessionId,
-      accessToken,
-    });
+    if (!mongoose.Types.ObjectId.isValid(sessionId)) {
+      return next(createHttpError(401, 'Invalid session'));
+    }
+
+    const session = await Session.findOne({ _id: sessionId, accessToken });
 
     if (!session) {
       return next(createHttpError(401, 'Invalid session'));
     }
 
-    if (new Date() > new Date(session.accessTokenValidUntil)) {
+    if (Date.now() > session.accessTokenValidUntil) {
       return next(createHttpError(401, 'Access token expired'));
     }
 
